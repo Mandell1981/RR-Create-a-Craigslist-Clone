3be99ec713f0ef35e6e34e67c4f5e82606a14883
Mandell1981 <Mandell1981.rm@gmail.com>
2024-02-18 13:07:07 -0500

    Start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // Import data
-
+import SearchBar from './searchbar'
 // Import components
 import './App.css';
 
 function App(){
   return (
     <div>
-      <h1>craigslist</h1>
+      <h1>Search Bar</h1>
       <div className="App">
         {/* Your content will go here! */}
       </div>
